140fb2caf79b46d401e26d695ab65934011db683
Seiei Higa <hanachin@gmail.com>
2013-03-02 03:32:41 +0900

    add token to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  before_create :set_uid
+  before_create :set_uid, :set_token
 
   has_many :watch_histories, dependent: :destroy
   has_many :watched_xvideos, through: :watch_histories, source: :xvideo
@@ -8,5 +8,9 @@ class User < ActiveRecord::Base
 
   def set_uid
     self.uid = SecureRandom.hex
+  end
+
+  def set_token
+    self.token = SecureRandom.hex
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130301164157) do
+ActiveRecord::Schema.define(:version => 20130301181410) do
 
   create_table "users", :force => true do |t|
     t.string   "uid"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "token"
   end
 
   create_table "watch_histories", :force => true do |t|
