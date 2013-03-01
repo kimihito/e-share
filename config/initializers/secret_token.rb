32b0057dab1b066a0945ed44ec364a7578baf0bb
kimihito <tatsurotamashiro@gmail.com>
2013-03-01 20:44:43 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EShare::Application.config.secret_token = '17d585de8f606d1d99413eb15f63e837df0d2ff5f5a65478c6fe97948dc9b9af46e2a52165dc7a249df3516cc5bfd5ac99ff3449ef671acc54dd241e834a5927'
