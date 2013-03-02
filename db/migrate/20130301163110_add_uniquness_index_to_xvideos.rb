4dafc61bbba3e0856029f7f1130a517121111b8e
Seiei Higa <hanachin@gmail.com>
2013-03-02 09:41:23 +0900

    デプロイ用にデータベースの設定をほげほげ

--- db/migrate/20130301163110_add_uniquness_index_to_xvideos.rb
+++ db/migrate/20130301163110_add_uniquness_index_to_xvideos.rb
@@ -1,5 +1,5 @@
 class AddUniqunessIndexToXvideos < ActiveRecord::Migration
   def change
-    add_index :xvideos, :url, unique: true
+    add_index :xvideos, :url, unique: true, length: {url:255}
   end
 end
